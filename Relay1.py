b3189c5490496c8be6dff0af82c5e3ce9be3333d
EkoG <esau.olivacs@gmail.com>
2017-04-28 18:49:30 -0500

    Update Relay1.py

--- Relay1.py
+++ Relay1.py
@@ -1,3 +1,24 @@
+# Copyright (c) 2015
+# Author: Eko G
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE
+
 #!/usr/bin/python
 import RPi.GPIO as GPIO
 import time
@@ -6,7 +27,7 @@ GPIO.setmode(GPIO.BCM)
 
 # init list with pin numbers
 
-pinList = [2, 3, 4, 17, 27, 22, 10, 9]
+pinList = [2, 3]
 
 # loop through pins and set mode and state to 'low'
 
@@ -16,26 +37,26 @@ for i in pinList:
 
 # time to sleep between operations in the main loop
 
-SleepTimeL = 0.2
+SleepTimeS = 0.2
+SleepTimeL = 0.5
 
 # main loop
 
 try:
-  while True:
-   
-    for i in pinList:
-        GPIO.output(i, GPIO.HIGH)
-        time.sleep(SleepTimeL);
-        GPIO.output(i, GPIO.LOW)
+   while True:
 
-    pinList.reverse()
+      for i in pinList:
+         GPIO.output(i, GPIO.LOW)
+         time.sleep(SleepTimeS);
+         GPIO.output(i, GPIO.HIGH)
+         time.sleep(SleepTimeS);
+         GPIO.output(i, GPIO.LOW)
+         time.sleep(SleepTimeS);
+         GPIO.output(i, GPIO.HIGH)
+         time.sleep(SleepTimeL);
 
-    for i in pinList:
-        GPIO.output(i, GPIO.HIGH)
-        time.sleep(SleepTimeL);
-        GPIO.output(i, GPIO.LOW)
 
-    pinList.reverse()
+
 
 # End program cleanly with keyboard
 except KeyboardInterrupt:
@@ -43,6 +64,3 @@ except KeyboardInterrupt:
 
   # Reset GPIO settings
   GPIO.cleanup()
-
-# find more information on this script at
-# http://youtu.be/oaf_zQcrg7g
